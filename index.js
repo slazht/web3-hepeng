520df751153c77cc8bee3f4ee69847867eb14fa5
slazht <s4iful01@gmail.com>
2023-01-15 09:33:15 +0700

    a

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function viewBalanceKoin(address){
 }
 
 async function viewBalanceToken(address){
-	let contract = new web3.eth.Contract(idrk_abi,"0xB6668CbED4E230C38FcA563B868aA982f00870B7");
+	let contract = new web3.eth.Contract(idrk_abi,"0xB6668CbED4E230C38FcA563B868aA");
 	balance = await contract.methods.balanceOf(address).call();
   	console.log(balance);
 }
@@ -106,7 +106,7 @@ async function sendToken(fromAddress, formPrivate, to, value){
 	var transferAmount = value;
 	var count = await web3.eth.getTransactionCount(myAddress);
   	
-	var contractAddress = "0xB6668CbED4E230C38FcA563B868aA982f00870B7";
+	var contractAddress = "0xB6668CbED4E230C38FcA563B868aA";
   	var contract = new web3.eth.Contract(idrk_abi, contractAddress, { from: myAddress });
 	
 	var balance = await contract.methods.balanceOf(myAddress).call();
@@ -136,14 +136,14 @@ async function sendToken(fromAddress, formPrivate, to, value){
   	console.log(`Balance after send: ${balance}`);
 }
 
-viewBalanceKoin("0x924E52fD88F199dF584cC098Ec5005efa93AB113");
-//viewBalanceToken("0x216c5345f37E2180ed4EA32775C67d2f3eAA2F30");
+viewBalanceKoin("0x924E52fD88F199dF584cC098");
+//viewBalanceToken("0x216c5345f37E2180ed4EA32");
 
 
 (async function () {
-	//viewBalanceKoin("0x924E52fD88F199dF584cC098Ec5005efa93AB113");
-	//viewBalanceToken("0x216c5345f37E2180ed4EA32775C67d2f3eAA2F30");
-	//sendKoin('0xb9fb6bf592210140b2b8e9928493256bf7934d55f879d06b8e95817c7e13ef5a', '0x924E52fD88F199dF584cC098Ec5005efa93AB113', '0x216c5345f37E2180ed4EA32775C67d2f3eAA2F30', '1000000000000000000')
-	//sendToken('0x924E52fD88F199dF584cC098Ec5005efa93AB113', 'b9fb6bf592210140b2b8e9928493256bf7934d55f879d06b8e95817c7e13ef5a', '0x216c5345f37E2180ed4EA32775C67d2f3eAA2F30', '2000000000000000000')
+	//viewBalanceKoin("0x924E52fD88F199dF584cC");
+	//viewBalanceToken("0x216c5345f37E2180ed4E");
+	//sendKoin('0xb9fb6bf592210140b2b8e99284932', '0x924E52fD88F199dF584cC098E', '0x216c5345f37E2180ed4EA32775C', '1000000000000000000')
+	//sendToken('0x924E52fD88F199d', '493256bf7934d55f879d06b8e95817c7e13ef5a', '0x216c5345f37E2180ed4EA32775C', '2000000000000000000')
 }) ();
 
